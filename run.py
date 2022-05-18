8520577e6e9450e220df046117766e1d68bed390
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-05-18 08:39:21 +0000

    Add start function

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print("hangman")
+def start():
+    play_game = True
+
+    while play_game:
+        continue_playing = input("Like to play? y/n \n")
+        if continue_playing.lower() == "y":
+            print("You have decided to play.")
+        elif continue_playing.lower() == "n":
+            print("Closing game.")
+            play_game = False
+        else:
+            print("That is not a valid option, Please try again.")
+
+
+start()
