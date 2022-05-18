4ff631dd0eca349d938e5b34c8b9807722422cda
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-05-18 09:29:18 +0000

    Set up basic play structure

--- run.py
+++ run.py
@@ -1,3 +1,46 @@
+guesses = []
+wrong_guesses = []
+
+def play():
+    word = "tester"
+
+    failed_attempts = 0
+
+    playing = True
+
+    while playing:
+        print(f"Failed attempts: {failed_attempts}/7\n")
+
+        for letter in word:
+            if letter.lower() in guesses:
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
+        print("")
+
+        guess = input("Choose a letter:\n")
+
+        guesses.append(guess.lower())
+
+        if guess.lower() not in word.lower():
+            failed_attempts += 1
+            wrong_guesses.append(guess)
+            if failed_attempts == 7:
+                break
+
+        playing = False
+
+        for letter in word:
+            if letter.lower() not in guesses:
+                playing = True
+
+    if not playing:
+        raise SystemExit(f"You found the word! It was: {word}")
+
+    else:
+        raise SystemExit(f"Game over the word was: {word}")
+
+
 def start():
     play_game = True
 
@@ -5,6 +48,7 @@ def start():
         continue_playing = input("Like to play? y/n \n")
         if continue_playing.lower() == "y":
             print("You have decided to play.")
+            play()
         elif continue_playing.lower() == "n":
             print("Closing game.")
             play_game = False
